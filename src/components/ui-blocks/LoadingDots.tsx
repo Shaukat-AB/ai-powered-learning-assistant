b2de004ebd82069d065b3134b86b2a24e9887d77
Shaukat Ali <Shaukatalibans@gmail.com>
2026-03-25 14:21:53 +0500

    feat (ui): add loading-dots component.

---
+++ src/components/ui-blocks/LoadingDots.tsx
@@ -0,0 +1,11 @@
+const LoadingDots = () => {
+  return (
+    <div className="flex items-center justify-center gap-1.5">
+      <span className="bg-primary size-2 animate-bounce rounded-full [animation-delay:-0.3s]" />
+      <span className="bg-primary size-2 animate-bounce rounded-full [animation-delay:-0.15s]" />
+      <span className="bg-primary size-2 animate-bounce rounded-full" />
+    </div>
+  );
+};
+
+export default LoadingDots;
