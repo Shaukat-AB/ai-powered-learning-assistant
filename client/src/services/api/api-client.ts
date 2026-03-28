f56fa238a5794cb2c23c76125f7224c19fa65755
Shaukat Ali <Shaukatalibans@gmail.com>
2026-03-28 11:23:09 +0500

    feat (client): this will add api-client for sending and receiving data from server.

---
+++ client/src/services/api/api-client.ts
@@ -0,0 +1,53 @@
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+export type TEndpoint = '/api/ai/start-chat' | '/api/ai/chat';
+
+export const apiClient = {
+  get: async (endpoint: TEndpoint) => {
+    const res = await fetch(BASE_URL + endpoint, {
+      method: 'GET',
+      credentials: 'include',
+    });
+
+    return await res.json();
+  },
+
+  post: async (endpoint: TEndpoint, data: unknown = null) => {
+    const res = await fetch(BASE_URL + endpoint, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data ? JSON.stringify(data) : null,
+      credentials: 'include',
+    });
+
+    return await res.json();
+  },
+
+  put: async (endpoint: TEndpoint, data: unknown = null) => {
+    const res = await fetch(BASE_URL + endpoint, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data ? JSON.stringify(data) : null,
+      credentials: 'include',
+    });
+
+    return await res.json();
+  },
+
+  delete: async (endpoint: TEndpoint, data: unknown = null) => {
+    const res = await fetch(BASE_URL + endpoint, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: data ? JSON.stringify(data) : null,
+      credentials: 'include',
+    });
+
+    return await res.json();
+  },
+};
