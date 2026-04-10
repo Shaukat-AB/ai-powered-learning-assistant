4e86acd5ec11d1d65c6e5109c703df0ab4bbfc90
Shaukat Ali <Shaukatalibans@gmail.com>
2026-04-10 13:00:49 +0500

    fix(client)(ui): this will fix as-child prop of button-with-arrow component and disable it if loading prop is true.

--- client/src/components/ui-blocks/ButtonWithArrow.tsx
+++ client/src/components/ui-blocks/ButtonWithArrow.tsx
@@ -1,13 +1,15 @@
 import { ArrowRightIcon } from 'lucide-react';
-import { Button } from '../ui/button';
-import { Activity } from 'react';
+
 import { Spinner } from '../ui/spinner';
+import { Button } from '../ui/button';
+
+import { Activity } from 'react';
 
 const ButtonWithArrow = ({
   className,
   variant = 'default',
   size = 'default',
-  // asChild = false,
+  asChild = false,
   loading = false,
   children,
   ...props
@@ -16,19 +18,22 @@ const ButtonWithArrow = ({
     <Button
       variant={variant}
       size={size}
-      // asChild={asChild}
+      asChild={asChild}
       className={`group ${className}`}
+      disabled={loading}
       {...props}
     >
-      {children}
+      <span className="inline-flex gap-1.5 shrink-0 items-center justify-center">
+        {children}
 
-      <Activity mode={loading ? 'visible' : 'hidden'}>
-        <Spinner />
-      </Activity>
+        <Activity mode={loading ? 'visible' : 'hidden'}>
+          <Spinner />
+        </Activity>
 
-      <Activity mode={!loading ? 'visible' : 'hidden'}>
-        <ArrowRightIcon className="transition-transform duration-200 group-hover:translate-x-0.5" />
-      </Activity>
+        <Activity mode={!loading ? 'visible' : 'hidden'}>
+          <ArrowRightIcon className="transition-transform duration-200 group-hover:translate-x-0.5" />
+        </Activity>
+      </span>
     </Button>
   );
 };
