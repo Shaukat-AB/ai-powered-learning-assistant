e73b004534943cb9d380c24303da665c6ea7e1a3
Shaukat Ali <Shaukatalibans@gmail.com>
2026-04-11 13:46:35 +0500

    refactor(client)(document-skeleton): this will use css document-card-w to define width.

--- client/src/components/documents/DocumentsSkeleton.tsx
+++ client/src/components/documents/DocumentsSkeleton.tsx
@@ -2,7 +2,7 @@ import { Skeleton } from '../ui/skeleton';
 
 const DocumentsSkeleton = () => {
   return Array.from({ length: 3 }).map((_, i) => (
-    <Skeleton className="w-full lg:max-w-xs sm:max-w-3xs h-42" key={i} />
+    <Skeleton className="w-full document-card-w h-42" key={i} />
   ));
 };
 
