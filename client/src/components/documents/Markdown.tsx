4b5d6372bb7a4e23b2dce44eceef40b8bb2d5c18
Shaukat Ali <Shaukatalibans@gmail.com>
2026-04-12 10:38:31 +0500

    refactor(client)(index.css): this will add css classes h1, h2, and h3 that extracted from markdown component of documents.

--- client/src/components/documents/Markdown.tsx
+++ client/src/components/documents/Markdown.tsx
@@ -7,26 +7,17 @@ const Markdown = ({ text = '' }) => {
     <ReactMarkdown
       components={{
         h1: ({ children, ...props }) => (
-          <h1
-            className="scroll-m-20 text-center text-4xl font-extrabold tracking-tight text-balance"
-            {...props}
-          >
+          <h1 className="h1" {...props}>
             {children}
           </h1>
         ),
         h2: ({ children, ...props }) => (
-          <h2
-            className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0"
-            {...props}
-          >
+          <h2 className="h2 border-b" {...props}>
             {children}
           </h2>
         ),
         h3: ({ children, ...props }) => (
-          <h3
-            className="scroll-m-20 text-2xl font-semibold tracking-tight"
-            {...props}
-          >
+          <h3 className="h3" {...props}>
             {children}
           </h3>
         ),
