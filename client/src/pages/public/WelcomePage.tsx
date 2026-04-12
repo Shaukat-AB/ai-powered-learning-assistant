15413a0d057422ea2f7782a3c016b172d037fae8
Shaukat Ali <Shaukatalibans@gmail.com>
2026-04-12 13:48:55 +0500

    refactor(client)(welcome-page): this will redesign and update styles and spacing of welcome page.

--- client/src/pages/public/WelcomePage.tsx
+++ client/src/pages/public/WelcomePage.tsx
@@ -1,4 +1,11 @@
-import { CardContent, CardTitle } from '@/components/ui/card';
+import { Sparkles } from 'lucide-react';
+
+import {
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 
 import HTMLSEO from '@/components/other/HTMLSEO';
 import ButtonWithArrow from '@/components/ui-blocks/ButtonWithArrow';
@@ -10,25 +17,45 @@ const WelcomePage = () => {
   const handleGoogleLogin = () => mutate();
 
   return (
-    <CardContent className="w-full h-full max-w-2xs flex items-center flex-col justify-center gap-5">
+    <CardContent className="relative overflow-hidden w-full flex sm:items-start flex-col justify-center gap-8">
       <HTMLSEO title="Welcome" />
 
-      <CardTitle className="text-2xl">Welcome</CardTitle>
-      <ButtonWithArrow
-        className="w-full"
-        size={'lg'}
-        variant={'outline'}
-        loading={isPending}
-        disabled={isPending}
-        onClick={handleGoogleLogin}
-      >
-        <img
-          src="https://cdn.shadcnstudio.com/ss-assets/brand-logo/google-icon.png?width=20&height=20&format=auto"
-          alt="Google Icon"
-          className="size-5"
-        />
-        <span className="flex flex-1 justify-center">Continue with Google</span>
-      </ButtonWithArrow>
+      <CardHeader className="w-full sm:text-left text-center space-y-2 max-w-lg">
+        <CardTitle className="h1 sm:text-left! font-semibold!">
+          <h1>Welcome Ready to grow?</h1>
+        </CardTitle>
+
+        <CardDescription>
+          <p>
+            Explore tailored pathways, chat with an AI tutor for on-demand help,
+            and generate quizzes that sharpen your skills, get started today.
+          </p>
+        </CardDescription>
+      </CardHeader>
+
+      <CardContent className="w-full flex">
+        <ButtonWithArrow
+          className="w-fit sm:mx-0 mx-auto px-8 py-6"
+          size={'lg'}
+          variant={'accent-alt'}
+          loading={isPending}
+          disabled={isPending}
+          onClick={handleGoogleLogin}
+        >
+          <img
+            src="https://cdn.shadcnstudio.com/ss-assets/brand-logo/google-icon.png?width=20&height=20&format=auto"
+            alt="Google Icon"
+            className="size-5"
+          />
+          <span className="flex flex-1 justify-center">
+            Continue with Google
+          </span>
+        </ButtonWithArrow>
+      </CardContent>
+
+      <div className="absolute -right-[6%] -z-10 overflow-hidden">
+        <Sparkles className="pointer-events-none size-[45vw] -rotate-8  text-muted-foreground/16" />
+      </div>
     </CardContent>
   );
 };
