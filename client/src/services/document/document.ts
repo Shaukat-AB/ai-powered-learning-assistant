193bd2b6c81b5b2b862eff41dcc929ab80a571c3
Shaukat Ali <Shaukatalibans@gmail.com>
2026-04-25 11:23:41 +0500

    refactor(client)(services-document): this will remove defining const data since it's unused also remove type from get-documents.

--- client/src/services/document/document.ts
+++ client/src/services/document/document.ts
@@ -1,14 +1,11 @@
-import type { TDocument } from '@/components/documents/types';
 import { apiClient } from '../api/api-client';
 
 export const getDocuments = async () => {
-  const data = await apiClient.get('/api/document');
-  return data as Array<TDocument>;
+  return await apiClient.get('/api/document');
 };
 
 export const uploadDocument = async (formData: FormData) => {
-  const data = await apiClient.postFile('/api/document/upload', formData);
-  return data;
+  return await apiClient.postFile('/api/document/upload', formData);
 };
 
 export const deleteDocument = async (name: string) => {
