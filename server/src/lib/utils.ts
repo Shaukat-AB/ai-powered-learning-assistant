9e6d6df7a8423fb2c1c07d913c77320d8972664e
Shaukat Ali <Shaukatalibans@gmail.com>
2026-04-27 14:29:50 +0500

    refactor(server)(tsconfig): this will map the shared folder to avoid relative path and update path in lib\utils.

--- server/src/lib/utils.ts
+++ server/src/lib/utils.ts
@@ -1,6 +1,6 @@
 import type { AppError } from './types.js';
 
-import { documentNameValidater } from '../../../shared/utils/document.js';
+import { documentNameValidater } from '@shared/utils/document.js';
 
 export const newError = (message = 'Internal Server Error', status = 500) => {
   const err = new Error(message) as AppError;
